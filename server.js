cfc1cee5751c1d74d91341c7037fcc257eb334b0
johannpa <johann_paque@hotmail.fr>
2025-03-04 17:49:46 +0100

    Connecting to the DB

--- server.js
+++ server.js
@@ -9,3 +9,6 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
     });
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nodebasiccrud');
